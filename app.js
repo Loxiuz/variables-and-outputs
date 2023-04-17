5a2008186f75be54e88b4f0fa6e937ea05953e92
Malte <maltebjoerklund@gmail.com>
2023-04-17 09:12:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,16 +9,33 @@ function start() {
   document.querySelector("#btn-increment").addEventListener("click", increment);
   document.querySelector("#btn-decrement").addEventListener("click", decrement);
 }
+
 function increment() {
   console.log("increment");
   number++;
   displayNumber();
 }
+
 function decrement() {
   console.log("decrement");
   number--;
   displayNumber();
 }
+
 function displayNumber() {
   document.querySelector("#number").textContent = number;
+
+  let message = "";
+
+  if (number > 10) {
+    console.log("number > 10");
+    message = "The number is above 10!";
+  } else if (number < 10) {
+    console.log("number < 10");
+    message = "The number is below 10!";
+  } else {
+    console.log("number = 10");
+    message = "The number is 10!";
+  }
+  document.querySelector("#message").textContent = message;
 }
